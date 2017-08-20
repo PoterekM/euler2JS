316ddbb0ebef538328575d64bfdba8fee32c81bf
Michelle Poterek <poterekm@gmail.com>
2017-08-20 15:43:21 -0700

    create variable for second to last variable in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
   var arrayOfSequence = [1, 2, 3, 8];
   var arrayLength = arrayOfSequence.length;
   arrayLength = (arrayLength - 1);
+  var arraySecondToLast = (arrayLength - 1);
   console.log(arrayLength);
   var lastValue = arrayOfSequence[(arrayLength)];
+  var secondToLastValue = arrayOfSequence[(arraySecondToLast)];
   console.log(lastValue);
+  console.log(secondToLastValue);
