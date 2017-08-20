cee995e25b351b5e63748a7e2ed04372b98a390f
Michelle Poterek <poterekm@gmail.com>
2017-08-20 15:39:51 -0700

    allow program to recognize last value of hard coded array

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+  var arrayOfSequence = [1, 2, 3, 8];
+  var arrayLength = arrayOfSequence.length;
+  arrayLength = (arrayLength - 1);
+  console.log(arrayLength);
+  var lastValue = arrayOfSequence[(arrayLength)];
+  console.log(lastValue);
