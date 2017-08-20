fc966ec6502ee00d184a7fec8389df6b24eb6cad
Michelle Poterek <poterekm@gmail.com>
2017-08-20 16:03:32 -0700

    push total  of last two values of array to the array of sequence

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,15 @@
   var arrayLength = arrayOfSequence.length;
   arrayLength = (arrayLength - 1);
   var arraySecondToLast = (arrayLength - 1);
-  console.log(arrayLength);
   var lastValue = arrayOfSequence[(arrayLength)];
+  console.log(typeof(lastValue));
   var secondToLastValue = arrayOfSequence[(arraySecondToLast)];
-  console.log(lastValue);
-  console.log(secondToLastValue);
+  console.log(typeof(secondToLastValue));
+  function add(lastValue, secondToLastValue) {
+    return lastValue + secondToLastValue;
+  }
+  console.log(typeof(secondToLastValue));
+  var total = add(lastValue, secondToLastValue);
+  console.log(total);
+  arrayOfSequence.push(total);
+  console.log(arrayOfSequence);
