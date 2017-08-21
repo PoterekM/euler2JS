f2bebc00a9c3f1d3faf2c7c0534d5127f63dc5dc
Michelle Poterek <poterekm@gmail.com>
2017-08-20 20:37:27 -0700

    implement cool trick for swapping two variabes in JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,37 @@
-var arrayOfSequence = [1, 2, 3, 5];
-var arrayLength = arrayOfSequence.length;
-arrayLength = (arrayLength - 1);
-var arraySecondToLast = (arrayLength - 1);
-var lastValue = arrayOfSequence[(arrayLength)];
-var secondToLastValue = arrayOfSequence[(arraySecondToLast)];
-function add(lastValue, secondToLastValue) {
-  return lastValue + secondToLastValue;
-}
-var newValue = add(lastValue, secondToLastValue);
-console.log(newValue);
+// var arrayOfSequence = [1, 2, 3, 5];
+// var arrayLength = arrayOfSequence.length;
+// arrayLength = (arrayLength - 1)
+// var arrayNumber = arrayOfSequence[arrayLength];
+// console.log(arrayNumber);
+// var arraySecondToLast = (arrayLength - 1);
+// var lastValue = arrayOfSequence[(arrayLength)];
+// var secondToLastValue = arrayOfSequence[(arraySecondToLast)];
+// function add(lastValue, secondToLastValue) {
+//   return lastValue + secondToLastValue;
+// }
+// var newValue = add(lastValue, secondToLastValue);
+//
+//
+//
+//
+//   for (var i = 0; i < 10; i++) {
+//     arrayOfSequence.push(newValue);
+//   }
+//
+//
+// console.log(arrayOfSequence);
 
-for (var i = 0; i < 10; i++) {
-  arrayOfSequence.push(newValue);
+var previous = 0,
+  current = 1,
+  sum = 0,
+  temp;
+
+while (current < 4000000) {
+  // if (current % 2 === 0)
+    sum += current;
+    temp = current;
+    current = current + previous;
+    previous = temp;
 }
-console.log(arrayOfSequence);
+
+console.log(sum);
