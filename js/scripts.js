1296b0a9a256cbb99fee009d209fb18ab135a81f
Michelle Poterek <poterekm@gmail.com>
2017-08-20 20:40:14 -0700

    change 4 mill to 4e6

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,14 @@
-// var arrayOfSequence = [1, 2, 3, 5];
-// var arrayLength = arrayOfSequence.length;
-// arrayLength = (arrayLength - 1)
-// var arrayNumber = arrayOfSequence[arrayLength];
-// console.log(arrayNumber);
-// var arraySecondToLast = (arrayLength - 1);
-// var lastValue = arrayOfSequence[(arrayLength)];
-// var secondToLastValue = arrayOfSequence[(arraySecondToLast)];
-// function add(lastValue, secondToLastValue) {
-//   return lastValue + secondToLastValue;
-// }
-// var newValue = add(lastValue, secondToLastValue);
-//
-//
-//
-//
-//   for (var i = 0; i < 10; i++) {
-//     arrayOfSequence.push(newValue);
-//   }
-//
-//
-// console.log(arrayOfSequence);
 
 var previous = 0,
-  index = 1,
+  current = 1,
   sum = 0,
   temp;
 
-while (index < 4000000) {
+while (index < 4e6) {
   if (index % 2 === 0)
-    sum += index;
-    temp = index;
-    index = index + previous;
+    sum += current;
+    temp = current;
+    current = current + previous;
     previous = temp;
 }
 
