0dc2ed5a8c9a7d778c069156472c171bf22c5c20
Michelle Poterek <poterekm@gmail.com>
2017-08-20 19:04:12 -0700

    return prgram back to better broken state before trying something new

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,16 @@
-  var arrayOfSequence = [1, 2, 3, 8];
-  var arrayLength = arrayOfSequence.length;
-  arrayLength = (arrayLength - 1);
-  var arraySecondToLast = (arrayLength - 1);
-  var lastValue = arrayOfSequence[(arrayLength)];
-  console.log(typeof(lastValue));
-  var secondToLastValue = arrayOfSequence[(arraySecondToLast)];
-  console.log(typeof(secondToLastValue));
-  function add(lastValue, secondToLastValue) {
-    return lastValue + secondToLastValue;
-  }
-  console.log(typeof(secondToLastValue));
-  var total = add(lastValue, secondToLastValue);
-  console.log(total);
-  arrayOfSequence.push(total);
-  console.log(typeof(arrayOfSequence));
-  console.log(arrayOfSequence);
-  console.log(typeof(arrayOfSequence));
+var arrayOfSequence = [1, 2, 3, 5];
+var arrayLength = arrayOfSequence.length;
+arrayLength = (arrayLength - 1);
+var arraySecondToLast = (arrayLength - 1);
+var lastValue = arrayOfSequence[(arrayLength)];
+var secondToLastValue = arrayOfSequence[(arraySecondToLast)];
+function add(lastValue, secondToLastValue) {
+  return lastValue + secondToLastValue;
+}
+var newValue = add(lastValue, secondToLastValue);
+console.log(newValue);
 
-
-  for (var index = 1; index < 4000000; index += 1) {
-    if ((lastValue % 2) !== 0 && (secondToLastValue % 2 !== 0) ) {
-      var total = add(lastValue, secondToLastValue)
-    }
-  }
-  alert(total);
-  console.log(total);
+for (var i = 0; i < 10; i++) {
+  arrayOfSequence.push(newValue);
+}
+console.log(arrayOfSequence);
